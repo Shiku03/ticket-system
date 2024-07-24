280a49e680e1dd91de51adbe3b5dd30c52c24bfe
Shiku03 <162791110+Shiku03@users.noreply.github.com>
2024-07-24 16:54:21 +0300

    connected files to database

--- partials/menu.php
+++ partials/menu.php
@@ -1,3 +1,20 @@
+<?php
+session_start();
+?>
+
+<?php 
+$servername="localhost";
+$username="root";
+$password="452bdgf@mysql";
+$database="ticket_system";
+
+$conn = new mysqli($servername,$username, $password, $database);
+
+if($conn -> connect_error){
+    echo "Connection Failed!".$conn->connect_error();
+}
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
